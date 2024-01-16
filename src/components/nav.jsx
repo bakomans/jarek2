440ae030b350cc415d472a49bae08b87faa42571
bakomans <bakomans@gmail.com>
2024-01-16 13:00:14 +0000

    name change

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Image src={logo} alt="Logo" className="logo" />
-      <Navbar.Brand as={Link} to="/" className="nav-brand">Jarosław Zywczak Geodezja</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/" className="nav-brand">Jarosław Żywczak Geodezja</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
       <BootstrapNav className="ml-auto animated-links text-center">
